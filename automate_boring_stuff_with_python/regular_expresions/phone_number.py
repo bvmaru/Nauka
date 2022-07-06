40c89b3fb14f9e984dd5aa7b875eaa6d429fefa7
Marcel <bvmaru@proton.me>
2022-07-06 14:06:45 +0200

    skrypt znajdujący w schowku wszystkie nr telefonu i adresy email, następnie wyodrebnia je od reszty tekstu i z powrotem umieszcza w schowku

--- automate_boring_stuff_with_python/regular_expresions/phone_number.py
+++ automate_boring_stuff_with_python/regular_expresions/phone_number.py
@@ -1,4 +1,39 @@
-import re
-phoneNumRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d')
-print(phoneNumRegex.findall('siema siema ffdsfdds 534-453-555, dsad 444-231-333'))
-#print('Znaleziono nr tel ' +mo.group())
+import pyperclip, re
+
+phoneRegex = re.compile(r'''(
+    (\+?\d{2})?
+    (\s|-|\.)?
+    (\d{3})                           
+    (\s|-|\.)?
+    (\d{3})                           
+    (\s|-|\.)?
+    (\d{3})                           
+)''',re.VERBOSE)
+
+mailRegex = re.compile(r'''(
+    [a-zA-Z0-9._%+-]+
+    @
+    [a-zA-Z0-9.-]+
+    (\.[a-zA-Z]{2,4})
+)''',re.VERBOSE)
+
+text = str(pyperclip.paste())
+matches =[]
+for groups in phoneRegex.findall(text):
+    if groups[1]:
+        phoneNum = "-".join([groups[1], groups[3], groups[5], groups[7]])
+    else:
+        phoneNum = "-".join([groups[3], groups[5], groups[7]])
+    matches.append(phoneNum)
+
+
+for groups in mailRegex.findall(text):
+    matches.append(groups[0])
+
+if len(matches)>0:
+    pyperclip.copy('\n'.join(matches))
+    print("Skopiowano do schowka: ")
+    print("\n".join(matches))
+else:
+    print("Nie znaleziono żadnego numeru telefonu lub adresu e-mail")
+
